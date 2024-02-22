93d1d788b1e81f8e5a0a5f2912746edfedd943e6
fran hernandez <franciscohernandezlopez5@gmail.com>
2024-02-22 09:28:35 +0100

    final challenge

--- scripts/module.js
+++ scripts/module.js
@@ -3,26 +3,37 @@ export const order = () => {
   const buttonNextOrder = document.querySelector('.next-order');
   const sizePizza = document.querySelector('.size-pizza');
   const kindDough = document.querySelector('.size-dough');
-  const finalPizza = document.querySelector('final-order')
+  const finalPizza = document.querySelector('.final-order')
+  const checkboxes = document.querySelectorAll('.ingredients-pizza input[type="checkbox"]')
+
+  const initialCheckBox = {}
+  checkboxes.forEach((checkbox) => {
+    initialCheckBox[checkbox.name] = checkbox.checked
+  })
 
   const onSubmit = (event) => {
     const ingredients = []
     event.preventDefault()
     const selectSize = sizePizza.value
     const selectDough = kindDough.value
-    document.querySelectorAll('.checkbox').forEach((checkbox) => {
-      if(checkbox.checked === true){
-        ingredients.push(checkbox.name)
-      }
-    })
-    if(ingredients.length >= 1) {
-      result.innerHTML = ` Tu pizza es de tamaño ${selectDough} con masa ${selectSize} y 
-      le pusiste ${ingredients.join(', ')} `
-      buttonNextOrder.value
-    }else {
-      result.innerHTML = `Debes seleccionar al menos un ingrediente`
+    
+    checkboxes.forEach((checkbox) => {
+       if(checkbox.checked === true){
+        ingredients.push(checkbox.value)
     }
+     checkbox.checked = initialCheckBox[checkbox.name]
+    }) 
+     if(ingredients.length >= 1) {
+      finalPizza.textContent = ` Tu pizza es de tamaño ${selectDough} con masa ${selectSize} y 
+      le pusiste ${ingredients.join(', ')} `
+      buttonNextOrder.innerHTML = 'Seguir pidiendo'
+      buttonCompleteOrder.innerHTML = 'Terminar pedido'
+    }else {
+      finalPizza.textContent = 'Debes seleccionar al menos un ingrediente'
+    }
+    sizePizza.value = sizePizza.options[0].value
+    kindDough.value = kindDough.options[0].value
   }
   buttonCompleteOrder.addEventListener('click', onSubmit)
-  buttonNextOrder.addEventListener('click', onsubmit)
+  buttonNextOrder.addEventListener('click', onSubmit)
 }
